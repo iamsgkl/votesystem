83685fec2a539d7e8210db6a55b8f8511be80139
Sagnik Lala <65915234+iamsgkl@users.noreply.github.com>
2021-09-01 21:45:49 +0530

    Add files via upload

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>Keep Calm, Vote From Home and Thank Project Group 41</b>
+    </div>
+    <strong>Copyright &copy; 2021 <a href="https://www.instagram.com/iamsgkl">Sagnik Lala</a></strong>
+</footer>
